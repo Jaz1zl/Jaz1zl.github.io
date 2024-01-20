71d329e1543b4294e764c4b21f1a9eae22568b42
Jaz1zl <aleksejsurkov03@gmail.com>
2024-01-20 18:16:36 +0300

    q

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ tg.MainButton.setText("Changed Text1"); //изменяем текст кнопк
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
+
+const button = document.getElementById('btn')
