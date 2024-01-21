36f1fb110e40537505ee4a100b71add8ced4bb04
Jaz1zl <aleksejsurkov03@gmail.com>
2024-01-21 23:14:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ button.onclick = function(){
     tg.openLink(meni_1)
 }
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    console.log(seconds)
+    // console.log(seconds)
+    tg.showAlert(seconds)
     if (seconds == 0) {
         var meni_1 = 'https://beruma.click/?i=16496';
         tg.openLink(meni_1)
