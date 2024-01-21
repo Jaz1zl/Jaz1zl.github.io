ff79bfde81aa2356df3fc07abe3ec3d4dbbe4cd4
Jaz1zl <aleksejsurkov03@gmail.com>
2024-01-21 23:29:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let tg = window.Telegram.WebApp
 tg.expand()
 
-tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.text = "Перейти 20..."; //изменяем текст кнопки 
+tg.MainButton.setText("Перейти 20..."); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#ffffff"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#2096c5"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
 var seconds = 5;
 
