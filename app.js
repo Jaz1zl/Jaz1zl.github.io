24d12427c1aa7eb1ba82dd53360a56237b6cf84c
Jaz1zl <aleksejsurkov03@gmail.com>
2024-03-08 20:31:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var meni_1 = 'https://1wxekk.com/casino/list?open=register#8a5v';
 let inputData =  tg.initData 
 // let check_tg = tg.viewportHeight
 
-let check_tg = tg.initDataUnsafe.user.isBot
+let check_tg =tg.initDataUnsafe.user.last_name
 console.log(inputData)
 console.log(check_tg)
 if (check_tg != true){
