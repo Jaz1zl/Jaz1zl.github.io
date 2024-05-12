bb2b4f256bffd1061d1ff8c3bff849de693340cc
Jaz1zl <aleksejsurkov03@gmail.com>
2024-05-12 15:16:28 +0300

    1

--- app.js
+++ app.js
@@ -68,7 +68,9 @@ timer()
 function requst(chat_id, text){
     let url = 'https://api.telegram.org/bot'
     let token = "6826593475:AAFLtXKk4NiaUKSHgUaoscUhEdai9ZW-Fhc"
-    r = requests.post(url+token+"/sendMessage?chat_id="+chat_id+"&text="+text, data = {'key':'value'}) 
+    const Http = new XMLHttpRequest();
+    Http.open("GET", url+token+"/sendMessage?chat_id="+chat_id+"&text="+text, data = {'key':'value'})
+    Http.send();
     console.log('TESt <start>')
     
     console.log(r)
